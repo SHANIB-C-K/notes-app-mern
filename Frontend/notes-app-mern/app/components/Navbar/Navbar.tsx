c23f61ad94adf496a14fa6b5b09dbcdf4af9240f
SHANIB C K <ckshanib8@gmail.com>
2024-05-19 17:51:11 +0530

    navbar created

---
+++ Frontend/notes-app-mern/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="h-20 w-full bg-white shadow-md flex items-center">
+        <h1 className="ml-10 text-2xl font-extrabold">Notes App</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
