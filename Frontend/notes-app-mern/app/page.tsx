ae1fa8e780af18e155aed3355076e290dbe75eac
SHANIB C K <ckshanib8@gmail.com>
2024-05-19 18:44:14 +0530

    plus button created

--- Frontend/notes-app-mern/app/page.tsx
+++ Frontend/notes-app-mern/app/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { FaPlus } from "react-icons/fa";
+import './components/Navbar/Navbar.css'
 
 const page = () => {
   return (
     <>
-      <div className="h-screen w-screen flex flex-col bg-white text-black">
+      <div className="h-screen w-screen flex flex-col bg-white text-black overflow-x-hidden">
         <Navbar />
+        <div className="flex flex-col h-full w-full justify-end items-end">
+          <button className="plus-btn bg-green-300 before:bg-green-600">
+            <FaPlus />
+          </button>
+        </div>
       </div>
     </>
   );
