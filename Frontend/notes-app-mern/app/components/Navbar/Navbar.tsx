a5a2282ff18d59e8cde70f9e728ee89ca2972a02
SHANIB-C-K <ckshanib8@gmail.com>
2024-05-26 18:16:44 +0530

    all errors are solved

--- Frontend/notes-app-mern/app/components/Navbar/Navbar.tsx
+++ Frontend/notes-app-mern/app/components/Navbar/Navbar.tsx
@@ -1,16 +1,29 @@
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { IoLogOut } from "react-icons/io5";
 
 const Navbar = () => {
-  const [username, setUsername] = useState(localStorage.getItem("username"));
+  const [username] = useState(localStorage.getItem("username"));
+  const router = useRouter();
+
+  const LogoutFunction = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("login");
+    router.push('/register')
+  };
 
   return (
     <>
       <div className="h-20 w-full bg-white shadow-md flex items-center justify-between fixed">
         <h1 className="ml-10 text-2xl font-extrabold">Notes App</h1>
         <div className="flex gap-4">
-          <p>Hello, <span className="font-bold">{username}</span></p>
-          <IoLogOut className="mr-10 text-2xl text-red-600 hover:scale-110 duration-300 hover:shadow-sm cursor-pointer" />
+          <p>
+            Hello, <span className="font-bold">{username}</span>
+          </p>
+          <IoLogOut
+            className="mr-10 text-2xl text-red-600 hover:scale-110 duration-300 hover:shadow-sm cursor-pointer"
+            onClick={LogoutFunction}
+          />
         </div>
       </div>
     </>
