44b11856561f1a2f11679b444fb6e8d2b82024ec
SHANIB-C-K <ckshanib8@gmail.com>
2024-06-02 19:36:55 +0530

    update and create page just design changed

--- Frontend/notes-app-mern/app/create/page.tsx
+++ Frontend/notes-app-mern/app/create/page.tsx
@@ -25,8 +25,8 @@ const create = () => {
     <>
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-0 m- sm:m-0 -rotate-6 rounded-3xl"></div>
+          <div className="relative px-4 m-10 py-10 bg-white shadow-lg rounded-3xl sm:m-0 sm:p-20">
             <div className="max-w-md mx-auto">
               <div>
                 <h1 className="text-2xl font-semibold">Add Notes</h1>
@@ -51,12 +51,11 @@ const create = () => {
                     </label>
                   </div>
                   <div className="relative">
-                    <input
+                    <textarea
                       autoComplete="off"
                       id="paragraph"
                       name="paragraph"
-                      type="text"
-                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      className="peer placeholder-transparent h-24 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 resize-none"
                       placeholder="paragraph"
                       onChange={(e) => setParagraph(e.target.value)}
                     />
